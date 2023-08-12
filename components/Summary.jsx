7e7533324d5675cadfdc19bc1b718fcfe2f8aff5
ManilaDas <maniladas411@gmail.com>
2023-08-12 18:28:11 +0530

    meow

--- components/Summary.jsx
+++ components/Summary.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 const Summary = () => {
   return (
     <div className="w-screen h-[100vh] flex flex-col items-center justify-center text-center text-white">
-        <div className="text-3xl">
-                Code Here Manila: Summary
+        <div className="text-3xl leading-relaxed mt-36">
+            <h1 className="text-blue-dark font-bold">Trainers get legal data for free <br/>from the community</h1><br/>
+            <h1 className="text-blue-mid font-bold">Community gets rewarded by <br/>providing their data</h1><br/>
+            <h1 className="text-blue-light font-bold ">Win Win for All</h1>
         </div>
     </div>
   )
