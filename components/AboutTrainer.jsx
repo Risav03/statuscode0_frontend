d7f568e9e21fff10dcba65b676c9406581c00dc1
Risav03 <needleweed03@gmail.com>
2023-08-12 19:32:40 +0530

    go

--- components/AboutTrainer.jsx
+++ components/AboutTrainer.jsx
@@ -8,9 +8,9 @@ const AboutTrainer = () => {
       <h1 className="text-blue-mid font-bold text-4xl mt-60">Searching for Training Data?</h1>
       <div className="text-[1rem] grid grid-flow-col grid-col-2 mt-32">
       <div>
-         <Image src={trainer} className="relative left-[30%] w-[60%]"/>
+         <Image src={trainer} className="relative left-[220px] w-[60%]"/>
       </div>
-      <div className="w-[60%] relative left-[10%]">
+      <div className="w-[60%] relative left-[200px]">
         <p className="text-right mt-12">Finding legal data for training data is quite a sensitive issue in the current world.<br/>
         <br/>
         What if people willingly provided their data to train you models.<br/>
--- components/Landing.jsx
+++ components/Landing.jsx
@@ -4,15 +4,15 @@ const Landing = () => {
   return (
     <div className="w-screen h-[100vh] flex flex-col items-center justify-center text-center text-white">
       <div className="grid grid-flow-col grid-col-2 mt-40">
-        <div className="text-4xl leading-relaxed relative right-[100px] mt-10">
-          <h1 className="text-blue-dark font-bold">Own your Data </h1>
+        <div className="text-[2.7rem] leading-relaxed relative right-[80px] mt-10">
+          <h1 className="text-blue-dark font-bold ">Own your Data </h1>
           <h1 className="text-blue-mid font-bold">Be in Control</h1>
           <h1 className="text-blue-light font-bold ">Start Earning</h1>
         </div>
-        <div className="relative left-[130px] border border-blue-mid rounded-xl px-16 bg-black/20">
-          <h2 className="text-blue-mid my-5">Dont <span className="text-white"> let</span> giants sell your data</h2>
-          <h2 className="text-blue-mid my-5">Upload <span className="text-white">your data and</span> own it <span className="text-white">in <br/>form of</span> NFTs</h2>
-          <h2 className="text-blue-mid my-5">Help train AI Models <span className="text-white">using your<br/> data and</span> earn</h2>
+        <div className="relative left-[130px] border border-blue-mid text-[1.2rem] rounded-xl px-24 bg-black/20 ">
+          <h2 className="text-blue-mid my-5"><b>Don't</b> <span className="text-white"> let</span> giants sell your data</h2>
+          <h2 className="text-blue-mid my-5">Upload <span className="text-white">your data and</span> own it <span className="text-white">in <br/>form of</span> <b>NFTs</b></h2>
+          <h2 className="text-blue-mid my-5">Help train AI Models <span className="text-white">using your<br/> data and</span> <b>earn</b></h2>
           <button className="bg-blue-mid hover:bg-blue-dark rounded-3xl p-2 px-4 font-bold mb-5">Connect Wallet</button>
         </div>
       </div>
