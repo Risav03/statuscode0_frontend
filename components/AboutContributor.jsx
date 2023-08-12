a57a3144166e39b5d5e02175ca8a91715f5000b5
ManilaDas <maniladas411@gmail.com>
2023-08-12 18:13:21 +0530

    go

--- components/AboutContributor.jsx
+++ components/AboutContributor.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import Image from "next/image";
+import contributor from "@assets/data.png"
 
 const AboutContributor = () => {
   return (
-    <div className="w-screen h-[100vh] flex flex-col items-center justify-center text-center text-white">
-      <div className="text-3xl">Code Here Manila: Contributor</div>
+    <div className="w-[80%] mx-auto h-[100vh] flex flex-col items-center justify-center text-center text-white">
+      <h1 className="text-blue-mid font-bold text-4xl mt-20">Want to be a Contributor?</h1>
+      <div className="text-[1rem] grid grid-flow-col grid-col-2 mt-32">
+        <div className="w-[70%] relative left-[10%]">
+          <p className="text-left mt-20">Most AI models are trained on copyrighted data without the permission of the creator.<br/><br></br>
+          Here you can help train AI models with your own datasets.<br/><br></br>
+          Upload and own your data in form of Non-Fungible Tokens.<br/><br></br>
+          Every time someone uses your data to train their model you get rewarded with our native currency token.</p>
+        </div>
+        <div>
+          <Image src={contributor} className="mx-auto w-[85%]"/>
+        </div>
+      </div>
     </div>
   );
 };
--- components/AboutTrainer.jsx
+++ components/AboutTrainer.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import Image from "next/image";
+import trainer from "@assets/ai.png"
 
 const AboutTrainer = () => {
   return (
     <div className="w-screen h-[100vh] flex flex-col items-center justify-center text-center text-white">
-      <div className="text-3xl">Code Here Manila: Trainer</div>
+      <h1 className="text-blue-mid font-bold text-4xl mt-60">Searching for Training Data?</h1>
+      <div className="text-[1rem] grid grid-flow-col grid-col-2 mt-32">
+      <div>
+         <Image src={trainer} className="relative left-[30%] w-[50%]"/>
+      </div>
+      <div className="w-[50%] relative ">
+        <p className="text-right mt-12">Finding legal data for training data is quite a sensitive issue in the current world.<br/>
+        <br/>
+        What if people willingly provided their data to train you models.<br/>
+        <br/>
+        You get a varied range of data for free and data providers get rewarded every time you download their data or share data.</p>
+      </div>
+      </div>
     </div>
   );
 };
