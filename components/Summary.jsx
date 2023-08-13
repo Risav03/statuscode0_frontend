9d5c8aa2bfc5a56800ddd9c5685ad9f7ad358f34
Risav03 <needleweed03@gmail.com>
2023-08-13 14:08:47 +0530

    ksydcadsdshgvc

--- components/Summary.jsx
+++ components/Summary.jsx
@@ -4,7 +4,7 @@ const Summary = () => {
   return (
     <div className="w-screen h-[100vh] flex flex-col items-center justify-center text-center text-white">
         <div className="text-3xl leading-relaxed mt-36">
-            <h1 className="text-blue-dark font-bold">Trainers get legal data for free <br/>from the community</h1><br/>
+            <h1 className="text-blue-dark font-bold">Trainers legally get data for free <br/>from the community</h1><br/>
             <h1 className="text-blue-mid font-bold">Community gets rewarded by <br/>providing their data</h1><br/>
             <h1 className="text-blue-light font-bold ">Win Win for All</h1>
         </div>
